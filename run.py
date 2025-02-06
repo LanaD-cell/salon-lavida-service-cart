c7866c21e0e0975116e72ee9f696ca89400aa934
LanaD-cell <rrwycmqu@students.codeinstitute.net>
2025-02-06 07:34:56 +0100

    pivet away from cart, create to-do list and create a dict for calling data instead of google sheet

--- run.py
+++ run.py
@@ -17,114 +17,38 @@ def connect_to_google_sheets():
     SHEET = GSPREAD_CLIENT.open('salon_lavida_pricelist')
     return SHEET
 
-# welcome
-
-print("Good morning Jo-Ann, lets make some money!\n")
-
-# create a date generator - (stackoverflow solution)
-
-my_date = {'date': datetime.datetime.now()} 
-
-#convert date to string
-
-my_date_str = my_date['date'].strftime('%Y-%m-%d %H:%M:%S') 
-
-class DateTimeEncoder(JSONEncoder):
-    """
-    create a date that is serializable to 
-    send to my worksheets, 
-    'https://pynative.com/python-serialize-datetime-into-json/'
-    """
-    def default(self, obj):
-        if isinstance(obj, (datetime.date)):
-            return obj.isoformat()
-
-# serializing date
-
-my_dateJSONData = json.dumps(my_date, cls=DateTimeEncoder)
-print(my_dateJSONData)
-
-#add to google worksheet
-SHEET = connect_to_google_sheets()
-daily_cost_worksheet = SHEET.worksheet("daily_cost")
-daily_profit_worksheet = SHEET.worksheet("daily_profit")
-
-#add to row
-
-daily_cost_worksheet.append_row([my_date_str])
-daily_profit_worksheet.append_row([my_date_str])
-print("Date added to worksheet\n")
-
-print("Please enter the clients name: \n")
-clients_name = input()
-
-def get_prices(SHEET):
-    """
-    pull the item pricing from the google sheet
-    """
-    price_list_worksheet = SHEET.worksheet("price_list")
-    items_and_prices = price_list_worksheet.get_all_records()
-    return{row['item']: row['price'] for row in items_and_prices}
-
-
-def display_cart(service_cart):
-    """
-    creating a basic shopping cart - I followed along with this video 
-    'Python Mini Project for Beginners | Build a Simple Python Shopping Cart App using LISTS'
-    """
-    if not service_cart:
-        print("Service cart is still empty")
-    else:
-        print("Service cart: \n")    
-    total_price = 0
-    for item in service_cart:
-        print(f"{item['name']}:${item['price']}")
-        total_price += item['price']
-    print(f"Total: ${total_price:}")
-
 def main():
-    prices = get_prices(SHEET)
-    total_prices = [] # file to store total price of each cart at end
-
+    service_to_do = [] # create a file to place the chosen items
+    """
+    Create a product dictionary with pricing and cost to draw data from for the to-do list
+    """
+    products_dict = {'20 Volume Oxide': (55, 15), 
+        '30 Volume Oxide':(65, 25), 
+        'Be Blond Lifting Powder': (35, 17), 
+        'Blowdry': (44, 0), 
+        'Time': (65, 0)
+        }
+  
     while True:
-        service_cart = [] # file to reset cart
-        total_price = 0
-
-        print("Service cart App")
-        print("1. 20 Volume Oxide")
-        print("2. 30 Volume Oxide ")
-        print("3. Be Blond Lifting Powder")
-        print("4. Time")
-        print("5. Blowdry")
-        print("6. Checkout")
+        print("\n===Service To-Do App===")
+        print("1. Add Products")
+        print("2. Show selected product list")
+        print("3. Remove Product from list")
+        print("3. Checkout")
         print("Enter 'q' to quit.\n")
 
-        choose = input ("Choose your products used (Products are in Portion quantities) or press 'q' to quit: \n")
+        choice = ("Enter your choice: ")
+        if choice == '1'
+            print()
+            n_service_to_do = int(input("How may task you want to add: "))
+            
+            for i in range(n_service_to_do):
+                task = input("Enter the product: ")
+                service_to_do.append({"products": service_to_do, "done": False})
+                print("Task added!")
+
         if choose.lower() == 'q':
-            total_prices.append(total_price)
-            print(f"Total earnings so far: {total_prices}\n")
             break
 
-        item_map = {
-            "1": "20 Volume Oxide",
-            "2": "30 Volume Oxide",
-            "3": "Be Blond Lifting Powder",
-            "4": "Time",
-            "5": "Blowdry"
-        }
 
-        if choose in item_map:
-            name = item_map[choose]
-            price = prices.get(name, "Price not available")
-            print(f"{name} costs ${price}\n")
-            service_cart.append({'name':name, 'price':price})
-            total_price += price
-        else:
-            print("Invalid Item chosen, please try again.")    
-
-        display_cart(service_cart)
-
-    print("Total Price and cost for service provided:", service_cart)    
-
-if __name__ == "__main__":
-    main()
+     
